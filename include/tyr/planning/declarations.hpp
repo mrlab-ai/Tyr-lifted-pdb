81ecd419b1046fa16d0f731b05899137b4d25459
Dominik Drexler <drexlerd93@gmail.com>
2026-02-25 11:59:54 +0100

    first projection

--- include/tyr/planning/declarations.hpp
+++ include/tyr/planning/declarations.hpp
@@ -89,6 +89,9 @@ class DefaultEventHandler;
 template<TaskKind Kind>
 using DefaultEventHandlerPtr = std::shared_ptr<DefaultEventHandler<Kind>>;
 }
+
+template<typename Task>
+class ExplicitProjection;
 }
 
 #endif
