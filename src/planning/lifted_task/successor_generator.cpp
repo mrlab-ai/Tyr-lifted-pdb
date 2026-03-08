f669372a0de9ec04b9a174b7f54d630fa25a80b2
Dominik Drexler <drexlerd93@gmail.com>
2026-03-08 19:26:18 +0100

    .

--- src/planning/lifted_task/successor_generator.cpp
+++ src/planning/lifted_task/successor_generator.cpp
@@ -136,7 +136,7 @@ void SuccessorGenerator<LiftedTask>::get_labeled_successor_nodes(const Node<Lift
 
                 const auto ground_action = fp::ground(action,
                                                       grounder_context,
-                                                      m_task->get_parameter_domains_per_cond_effect_per_action()[uint_t(action.get_index())],
+                                                      m_task->get_parameter_domains_per_cond_effect_per_action().at(action_index),
                                                       fluent_assign,
                                                       iter_workspace,
                                                       *m_task->get_fdr_context())
--- src/planning/lifted_task/task_grounder.cpp
+++ src/planning/lifted_task/task_grounder.cpp
@@ -322,7 +322,7 @@ GroundTaskPtr ground_task(LiftedTask& lifted_task, ExecutionContext& execution_c
 
                 const auto ground_action = fp::ground(action,
                                                       grounder_context,
-                                                      lifted_task.get_parameter_domains_per_cond_effect_per_action()[uint_t(action.get_index())],
+                                                      lifted_task.get_parameter_domains_per_cond_effect_per_action().at(action_index),
                                                       fluent_assign,
                                                       iter_workspace,
                                                       *lifted_task.get_fdr_context())
