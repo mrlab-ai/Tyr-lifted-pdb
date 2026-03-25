d4ea68efa904eeb0ed3886c20bfd45cb26f2c50d
Dominik Drexler <drexlerd93@gmail.com>
2026-03-25 11:13:13 +0100

    finish rebase

--- src/planning/lifted_task/heuristics/rpg_ff.cpp
+++ src/planning/lifted_task/heuristics/rpg_ff.cpp
@@ -136,7 +136,7 @@ void FFRPGHeuristic<LiftedTag>::extract_relaxed_plan_and_preferred_actions(forma
 
         const auto ground_action = formalism::planning::ground(action,
                                                                grounder_context,
-                                                               m_task->get_parameter_domains_per_cond_effect_per_action().at(uint_t(action.get_index())),
+                                                               m_task->get_parameter_domains_per_cond_effect_per_action().at(action.get_index()),
                                                                m_assign,
                                                                m_iter_workspace,
                                                                *m_task->get_fdr_context())
