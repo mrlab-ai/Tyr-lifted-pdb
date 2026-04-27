ef85a191748f944464fa7ea3d43f7a5288b83959
Jordan Thayer <jordan.thayer@gmail.com>
2026-04-27 06:26:26 +0200

    with the bug fixed, no need for minimum reproducer

--- reproduce_assert_fail.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/home/jorth68/research/Tyr-lifted-pdb/build/exe/astar_eager \
-  -D data/pathways/domain.pddl \
-  -P data/pathways/test_problem.pddl \
-  -H projection_abstraction_first \
-  -N 1
